521911a13e99a3f7734d938e42c3fe97e2b804bf
insomnia1337 <mateusz.bieda95@gmail.com>
2018-04-07 20:02:24 +0200

    webpack styles fix

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        },
+          {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: 'file-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
+  };
