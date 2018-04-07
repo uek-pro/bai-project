56cf8b472d8ca78e8d2daa9b9a15ffb71dc48b70
Mateusz <mateusz.bieda95@gmail.com>
2018-04-07 23:29:30 +0200

    Webpack configuration (#8)
    * webpack styles fix
    * Update package.json
    * Change webpack output directory

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import style from "./main.scss";
+
+
+console.log(`I'm so bored...`)
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
