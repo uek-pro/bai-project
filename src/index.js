78654011ad7bdad61e28f7f70e3138f6ee3a3aaa
danbraj <danbraj@users.noreply.github.com>
2018-04-20 21:09:10 +0200

    Play with graphs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import style from "./main.scss";
 
 import firebase from 'firebase';
 import firebaseConfig from './components/firebase/firebase_config.js';
+import chart from 'chart.js';
 
 import { logIn, createAccount } from './components/firebase/authentication/traditional/auth.js';
 import socialMediaLogIn from './components/firebase/authentication/social_media/auth.js';
 import { logOut, checkAuth, notifyAuthStateChanged } from './components/firebase/authentication/common_auth.js';
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -31,5 +34,96 @@ notifyAuthStateChanged(function (user) {
     } else {
         window.location.hash = '';
         console.log('Signed off.');
+    }
+});
+
+
+var data1 = {
+    datasets: [
+        {
+            data: [23, 4],
+            backgroundColor: ['green', 'red']
+        }
+    ],
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Wykonano',
+        'Nie wykonano'
+    ]
+};
+
+var ctx1 = document.getElementById('myChart1').getContext('2d');
+var myDoughnutChart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: data1,
+    options: {
+        title: {
+            display: true,
+            fontSize: 16,
+            text: '% skuteczności'
+        },
+        legend: false,
+        events: null
+    }
+});
+
+
+var data2 = {
+    labels: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+    datasets: [{
+        fill: 'start',
+        backgroundColor: 'green',
+        data: [3, 0, 2, 6, 1, 9, 0, 5, 7]
+    }]
+};
+
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var myLineChart = new Chart(ctx2, {
+    type: 'line',
+    data: data2,
+    options: {
+        title: {
+            display: true,
+            fontSize: 16,
+            text: 'Wykres'
+        },
+        legend: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        elements: {
+            line: {
+                tension: 0
+            }
+        }
+    }
+});
+
+
+var data3 = {
+    datasets: [
+        {
+            data: [12, 8, 3],
+            backgroundColor: ['green', 'orange', 'red']
+        }
+    ]
+};
+
+var ctx3 = document.getElementById('myChart3').getContext('2d');
+var myDoughnutChart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: data3,
+    options: {
+        title: {
+            display: true,
+            fontSize: 16,
+            text: '% skuteczności'
+        },
+        legend: false,
+        events: null
     }
 });
