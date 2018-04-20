79318e9a6a3fb377be88fc69ef47c0d79f1c2432
danbraj <danbraj@users.noreply.github.com>
2018-04-20 13:39:26 +0200

    Prepare 'add habit' form

---
+++ src/components/firebase/appdata/habits_manager.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase';
+
+const showHabits = () => {
+
+    // pobierz zwyczaje
+
+    return true;
+}
+
+const addHabit = (habit) => {
+
+    console.log(habit);
+    // pobierz listę
+    // dodaj do listy obiekt habit
+    // zapis listę
+
+    return true;
+};
+
+const editHabit = () => {
+
+    // wyszukaj habit
+    // usun habit
+    // dodaj habit
+
+    return true;
+};
+
+const deleteHabit = () => {
+
+    // wyszukaj pasujący habit (wszystkie pola powinny być takie same)
+    // array.splice(index, 1)
+
+    return true;
+};
+
+export { showHabits, addHabit, editHabit, deleteHabit };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import firebaseConfig from './components/firebase/firebase_config.js';
 import { logIn, createAccount } from './components/firebase/authentication/traditional/auth.js';
 import socialMediaLogIn from './components/firebase/authentication/social_media/auth.js';
 import { logOut, checkAuth, notifyAuthStateChanged } from './components/firebase/authentication/common_auth.js';
+import { addHabit } from "./components/firebase/appdata/habits_manager";
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -25,6 +27,50 @@ document.querySelector('#btnLogOut').addEventListener('click', logOut);
 document.querySelector('#btnGoogleLogIn').addEventListener('click', () => socialMediaLogIn('google'));
 document.querySelector('#btnGitHubLogIn').addEventListener('click', () => socialMediaLogIn('github'));
 document.querySelector('#btnFacebookLogIn').addEventListener('click', () => socialMediaLogIn('facebook'));
+
+
+// PAGE: LOGOWANIE (STRONA GŁÓWNA)
+// PAGE: REJESTRACJA [OKNO MODALNE]
+// PAGE: STRONA Z LISTĄ ZADAŃ [PODSTRONA]
+// PAGE: STRONA GLOBALNEGO PODSUMOWANIA [PODSTRONA]
+// PAGE: STRONA USTAWIEŃ [PODSTRONA]
+// PAGE: STRONA Z SUGEROWANYMI ZADANIAMI
+// PAGE: STRONA DODAWANIA ZADANIA
+const mhTitle = document.getElementById('manageHabit-title');
+const mhDescription = document.getElementById('manageHabit-description');
+const mhType = document.getElementById('manageHabit-type');
+
+const mhOptimalValue = document.getElementById('manageHabit-optimal-value');
+const mhAuthor = document.getElementById('manageHabit-author');
+// TODO: + słowniczek
+
+document.getElementById('manageHabit-add-btn').addEventListener('click', () => {
+
+    const habitType = +mhType.value;
+    if (habitType >= 0 && habitType < 4) {
+
+        const habit = {
+            name: mhTitle.value,
+            desc: mhDescription.value,
+            type: habitType
+        }
+
+        if (habitType == 1) {
+            habit.optimal = +mhOptimalValue.value;
+        } else if (habitType == 2) {
+            habit.author = mhAuthor.value
+        } else {
+            // TODO: słowniczek
+        }
+
+        addHabit(habit);
+    }
+});
+
+// PAGE: ? STRONA EDYCJI ZADANIA ?
+// PAGE: TODO: STRONA WIDOKU POJEDYNCZEGO ZADANIA
+// PAGE: STRONA REALIZACJI ZADANIA [ODPALANA AUTOMATYCZNIE O OKREŚLONEJ PORZE]
+
 
 notifyAuthStateChanged(function (user) {
     if (user) {
